6f2096c1aae91c1b162cb6be2ed51647677dec02
Usheninte <jrandangana@gmail.com>
2019-05-13 16:14:30 +0100

    building buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -6,12 +6,13 @@ export default class Buttons extends Component {
       <div className="container-fluid">
         <div className="row">
           <div className="col">
-          
+            <span>1</span>
           </div>
           <div className="col">
-            
+            <span>2</span>
           </div>
           <div className="col">
+            <span>3</span>
           </div>
         </div>
       </div>

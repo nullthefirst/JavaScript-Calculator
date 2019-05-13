32b93da22bce9b168d3b7bb70dc7f4932c5ec652
Usheninte <jrandangana@gmail.com>
2019-05-13 15:44:18 +0100

    edited html

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReactFCCtest from 'react-fcctest';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <ReactFCCtest />
       <h1>fCC JavaScript Calculator</h1>
     </div>
   );

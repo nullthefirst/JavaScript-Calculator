93b6b461fad8c2a2cbadbb676fa7332045a953ef
Usheninte <jrandangana@gmail.com>
2019-05-13 16:01:18 +0100

    added bootstrap cdn

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactFCCtest from 'react-fcctest';
 import './App.css';
 
+// Components
+import Case from './components/Case';
+
 function App() {
   return (
     <div>
       <ReactFCCtest />
-      <h1>fCC JavaScript Calculator</h1>
+      <Case />
     </div>
   );
 }
